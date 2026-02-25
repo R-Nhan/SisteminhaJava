76dfd9e69ad334a27df1e81b84ad9967581fd1a5
R-Nhan <rubensiangbi@gmail.com>
2026-02-25 08:43:34 -0300

    Questao 03 consertada

--- Questao03/Main.java
+++ Questao03/Main.java
@@ -1,24 +1,27 @@
-//03) Uma rainha requisitou os serviços de um monge, o qual exigiu o pagamento em grãos de trigo da seguinte maneira: os grãos de trigo seriam dispostos em um tabuleiro de xadrez, de tal forma que a primeira casa do tabuleiro tivesse um grão, e as casas seguintes o dobro da anterior. Construa um algoritmo que calcule quantos grãos de trigo a Rainha deverá pagar ao monge.
 package Exercicios.Questao03;
+// Uma rainha requisitou os serviços de um monge, 
+// o qual exigiu o pagamento em grãos de trigo da seguinte maneira: 
+// os grãos de trigo seriam dispostos em um tabuleiro de xadrez, 
+// de tal forma que a primeira casa do tabuleiro tivesse um grão, 
+// e as casas seguintes o dobro da anterior. 
+// Construa um algoritmo que calcule quantos grãos de trigo a 
+// Rainha deverá pagar ao monge.
+
+import java.math.BigInteger;
+import java.text.NumberFormat;
 
 public class Main {
     public static void main(String[] args) {
-        
-        long aux;
+        BigInteger graos = BigInteger.ONE;
+        BigInteger soma = graos;
 
-        System.out.println("===================");
-        System.out.println("PAGAMENTOS EM GRÃOS");
-        System.out.println("===================");
-
-        aux = 1;
-
-        for (int i=1; i < 64; i++) {
-            aux = aux * 2 + 1;
+        for (int i = 2; i <= 64 ; i++) {
+            graos = graos.multiply(BigInteger.TWO);
+            soma = soma.add(graos);
         }
 
-
-
-        System.out.println("Total de grãos: " + aux);
-
+        NumberFormat formatador = NumberFormat.getInstance();
+        String somaFormatada = formatador.format(soma);
+        System.out.print("A Rainha devera pagar " + somaFormatada + " graos.");
     }
 }
